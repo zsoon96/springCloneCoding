efed01a51fcf2336ec6970f7655d42e889e4d53b
chIorophyII <dab2in79@gmail.com>
2022-04-15 01:45:20 +0900

    first commit

---
+++ src/main/java/com/sparta/springclonecoding/SpringclonecodingApplication.java
@@ -0,0 +1,13 @@
+package com.sparta.springclonecoding;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringclonecodingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringclonecodingApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/sparta/springclonecoding/SpringclonecodingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sparta.springclonecoding;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringclonecodingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
