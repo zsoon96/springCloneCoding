47859ab2b5421ce55acc3c74e1b07eb3bba51927
chIorophyII <dab2in79@gmail.com>
2022-04-15 19:56:25 +0900

    d

--- src/main/java/com/sparta/springclonecoding/model/Post.java
+++ src/main/java/com/sparta/springclonecoding/model/Post.java
@@ -1,40 +1,40 @@
-//package com.sparta.springclonecoding.model;
-//
-//import com.fasterxml.jackson.annotation.JsonFormat;
-//import lombok.Getter;
-//import lombok.NoArgsConstructor;
-//import org.springframework.data.annotation.CreatedDate;
-//
-//import javax.persistence.*;
-//import java.time.LocalDateTime;
-//import java.util.ArrayList;
-//import java.util.List;
-//
-//@Getter
-//@Entity
-//@NoArgsConstructor
-//public class Post {
-//    @Id
-//    @GeneratedValue(strategy = GenerationType.IDENTITY)
-//    @Column(name = "post_id")
-//    private Long id;
-//
-//    private Long userId;
-//
-//    private String imageUrl;
-//
-//    private String content;
-//
-//    @OneToMany
-//    @JoinColumn(name = "comment_id")
-//    private List<Comment> comments = new ArrayList<>();
-//
-//    @OneToMany
-//    @JoinColumn(name = "like_id")
-//    private List<Like> likes = new ArrayList<>();
-//
-//    @CreatedDate
-//    @JsonFormat(pattern = "yyyy-MM-dd")
-//    private LocalDateTime createdAt;
-//
-//}
+package com.sparta.springclonecoding.model;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.CreatedDate;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+
+@Getter
+@Entity
+@NoArgsConstructor
+public class Post {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column
+    private Long id;
+
+    private Long userId;
+
+    private String imageUrl;
+
+    private String content;
+
+    @OneToMany
+    @JoinColumn
+    private List<Comment> comments = new ArrayList<>();
+
+    @OneToMany
+    @JoinColumn
+    private List<Like> likes = new ArrayList<>();
+
+    @CreatedDate
+    @JsonFormat(pattern = "yyyy-MM-dd")
+    private LocalDateTime createdAt;
+
+}
